ac5c5f976d7ca7883ca55601eec7aae7f9df2a98
JohanaFigueredo <johana.carolina.figueredo@gmail.com>
2022-02-23 11:26:36 -0400

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace PruebaGalac
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_LeerArchivo = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_LeerArchivo
+            // 
+            this.btn_LeerArchivo.Location = new System.Drawing.Point(444, 257);
+            this.btn_LeerArchivo.Name = "btn_LeerArchivo";
+            this.btn_LeerArchivo.Size = new System.Drawing.Size(75, 23);
+            this.btn_LeerArchivo.TabIndex = 0;
+            this.btn_LeerArchivo.Text = "Leer Archivo";
+            this.btn_LeerArchivo.UseVisualStyleBackColor = true;
+            this.btn_LeerArchivo.Click += new System.EventHandler(this.btn_LeerArchivo_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(50, 58);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(469, 165);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Palabras Guardadas ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(627, 354);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_LeerArchivo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_LeerArchivo;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+using System.Windows.Forms;
+
+namespace PruebaGalac
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_LeerArchivo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                LeerArvhivo();
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.Message);
+                RegistrarEvento(0, " Error", "E", "Error: " + ex.Message.ToString());
+            }
+            finally
+            {
+                
+            }
+        }//fin btn
+
+        public void LeerArvhivo()
+        {
+            try
+            {
+                //variables
+                string PalabraconS = "";
+                string Palabrafinal = "";
+                string PalabrasLinea = "";
+                int contador = 0;
+                //rutas de archivos de entrada y salida
+                string RUTA = ConfigurationManager.AppSettings["RUTA_ARCHIVOS"].ToString();
+                string ArchivoEntrada = ConfigurationManager.AppSettings["NOMBRE_ARCHIVO_ENTRADA"].ToString();
+                string ArchivoSalida = ConfigurationManager.AppSettings["NOMBRE_ARCHIVO_SALIDA"].ToString(); ;
+
+                StreamReader Archivo = new StreamReader(RUTA + ArchivoEntrada);
+
+                //leo la primera linea
+                string linea = Archivo.ReadLine();
+                //recorro el archivo entrada hasta el final
+                while (linea != null)
+                {   
+                    //guardo la linea
+                    contador++;
+
+                    //rrecorro la linea palabra por palabra
+                    List<string> list = new List<string>();
+                    list = linea.Split(' ').ToList();
+
+                    foreach (string palabra in list)
+                    {
+                        //evaluo sila palabra contiene la letra S
+                        bool esta = palabra.ToUpper().Contains("S");
+                        if (esta)
+                        {
+                            Palabrafinal = palabra;
+                            //elimino los caracteres especiales . ? , !
+                            string[] charsToRemove = new string[] { "?", ",", ".", "!", "'" };
+                            foreach (var c in charsToRemove)
+                            {
+                                Palabrafinal = Palabrafinal.Replace(c, string.Empty);
+                            }
+                            if (contador == 1)
+                                PalabraconS = Palabrafinal;
+                            else
+                                PalabraconS = PalabraconS + ";" + Palabrafinal;
+                            
+                            PalabrasLinea = PalabrasLinea + "," + Palabrafinal;
+                        }
+                    }
+                    //guardo las palabras encontradas en log de BD
+                    RegistrarEvento(contador, PalabrasLinea, "I", "Palabras encontradas con la letra S");
+                    PalabrasLinea = "";
+
+                    //paso a la linea siguiente
+                    linea = Archivo.ReadLine();
+                }
+                //cierro el archivo
+                Archivo.Close();
+                File.WriteAllText(RUTA + ArchivoSalida, PalabraconS);
+
+                //Guardo en el log de BD
+                RegistrarEvento(0, " Informacion", "I", "Archivo " + ArchivoSalida +" leído exitosamente");
+
+                //llamo a la funcion para cargar el datagrid con las palabras guaradadas en el dia de ejecucion
+                Mostrar();
+            }
+            
+            catch (Exception ex)
+            {
+                RegistrarEvento(0, " Error", "E", "Error: " + ex.Message.ToString());
+             }
+            finally
+            {
+                MessageBox.Show("Proceso finalizado");
+                RegistrarEvento(0, " Informacion", "I", "Proceso finalizado.");
+            }
+        }//fin LeerArvhivo
+
+
+        public DataSet  RegistrarEvento(int Linea, string PalabrasEncontradas, string TipoEvento, string DescripcionEvento)
+        {
+            SqlConnection oConnection = null;
+            SqlCommand oCommand = null;
+            SqlDataAdapter oAdapter = null;
+            DataSet oVerify = new DataSet();
+            string sParam = string.Empty;
+            string sConnection = string.Empty;
+
+            try
+            {
+                //me conecto a bd
+                sConnection = ConfigurationManager.ConnectionStrings["CONECCION_BASE_DATOS"].ToString();
+                oConnection = new SqlConnection(sConnection);
+                oConnection.Open();
+
+                //ejecuto sp para guardar registro en BD
+                oCommand = new SqlCommand("SP_RegistrarEvento", oConnection);
+                oCommand.CommandType = CommandType.StoredProcedure;
+                oCommand.Parameters.AddWithValue("@Linea", Linea);
+                oCommand.Parameters.AddWithValue("@PalabrasEncontradas", PalabrasEncontradas);
+                oCommand.Parameters.AddWithValue("@idTipoEvento", TipoEvento);
+                oCommand.Parameters.AddWithValue("@DescripcionEvento", DescripcionEvento);
+                oAdapter = new SqlDataAdapter(oCommand);
+                oAdapter.Fill(oVerify);
+                return oVerify;
+            }
+            catch (Exception ex)
+            {
+                RegistrarEvento(0, "Error", "E", "Error en RegistrarEvento: " + ex.Message.ToString());
+                throw new Exception(ex.Message, ex);
+            }
+            finally
+            {
+                if (oConnection != null)
+                {
+                    oConnection.Close();
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        public DataSet Mostrar()
+        {
+            SqlConnection oConnection = null;
+            SqlCommand oCommand = null;
+            DataSet oVerify = new DataSet();
+            SqlDataAdapter oAdapter = null;
+            string sParam = string.Empty;
+            string sConnection = string.Empty;
+
+            try
+            {   
+                //me conecto a BD
+                sConnection = ConfigurationManager.ConnectionStrings["CONECCION_BASE_DATOS"].ToString();
+                oConnection = new SqlConnection(sConnection);
+                oConnection.Open();
+
+                oCommand = new SqlCommand("SP_MostrarReg", oConnection);
+                oCommand.CommandType = CommandType.StoredProcedure;
+
+                oAdapter = new SqlDataAdapter(oCommand);
+                oAdapter.Fill(oVerify);
+
+                //lleno DGV
+                dataGridView1.AutoGenerateColumns = true;
+                dataGridView1.DataSource = oVerify.Tables[0];
+                dataGridView1.Visible = true;
+
+                RegistrarEvento(0, " Informacion", "I", "Se ejecuto la funcion Mostrar");
+
+                return oVerify;
+            }
+            catch (Exception ex)
+            {
+                RegistrarEvento(0, " Error", "E", "Error en Mostrar: " + ex.Message.ToString());
+                throw new Exception(ex.Message, ex);
+            }
+            finally
+            {
+                if (oConnection != null)
+                {
+                    oConnection.Close();
+                }
+            }
+        }
+
+    }
+}
